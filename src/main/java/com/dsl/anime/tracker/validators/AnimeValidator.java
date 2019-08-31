a522cad5f43e4aca6837647bf345d80f42d587fa
dslsyeoh <syeoh91x@gmail.com>
2019-09-01 01:14:52 +0800

    remove unused @Component

--- src/main/java/com/dsl/anime/tracker/validators/AnimeValidator.java
+++ src/main/java/com/dsl/anime/tracker/validators/AnimeValidator.java
@@ -4,12 +4,10 @@ import com.dsl.anime.tracker.constraints.Anime;
 import com.dsl.anime.tracker.entity.AnimeEntity;
 import com.dsl.anime.tracker.repository.AnimeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-@Component
 public class AnimeValidator implements ConstraintValidator<Anime, AnimeEntity>
 {
     @Autowired
