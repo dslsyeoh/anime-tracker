b8bacd9933f3ffc2c0dd70a0d4d4d7ec4f1a8adb
dslsyeoh <syeoh91x@gmail.com>
2019-09-01 01:10:41 +0800

    add new method findByName

--- src/main/java/com/dsl/anime/tracker/repository/AnimeRepository.java
+++ src/main/java/com/dsl/anime/tracker/repository/AnimeRepository.java
@@ -4,7 +4,10 @@ import com.dsl.anime.tracker.entity.AnimeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AnimeRepository extends JpaRepository<AnimeEntity, Long>
 {
+    Optional<AnimeEntity> findByName(String name);
 }
