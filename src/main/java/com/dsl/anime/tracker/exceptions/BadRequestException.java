9a09b527cc1a3dcf8ba09cb666ca61464302c750
dslsyeoh <syeoh91x@gmail.com>
2019-09-02 00:23:31 +0800

    enhance BadRequestException

--- src/main/java/com/dsl/anime/tracker/exceptions/BadRequestException.java
+++ src/main/java/com/dsl/anime/tracker/exceptions/BadRequestException.java
@@ -1,9 +1,31 @@
 package com.dsl.anime.tracker.exceptions;
 
+import com.dsl.anime.tracker.exceptions.dto.Violation;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolation;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BadRequestException extends RuntimeException
 {
-    public BadRequestException()
+    private List<Violation> errors ;
+
+    public <T> BadRequestException(Set<ConstraintViolation<T>> constraintViolations)
     {
-        super("Bad request");
+        super();
+        errors = constraintViolations.stream().map(constraintViolation -> {
+            Violation violation = new Violation();
+            violation.setField(constraintViolation.getPropertyPath().toString());
+            violation.setErrors(Collections.singletonList(constraintViolation.getMessage()));
+            return violation;
+        }).collect(Collectors.toList());
+    }
+
+    public List<Violation> getErrors()
+    {
+        return errors;
     }
 }
