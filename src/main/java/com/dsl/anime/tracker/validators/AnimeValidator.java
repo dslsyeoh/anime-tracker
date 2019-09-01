5fced217f0ec1d07238e7ef128ab1b5a5ddbb83d
dslsyeoh <syeoh91x@gmail.com>
2019-09-01 19:38:47 +0800

    refactor validator

--- src/main/java/com/dsl/anime/tracker/validators/AnimeValidator.java
+++ src/main/java/com/dsl/anime/tracker/validators/AnimeValidator.java
@@ -3,19 +3,28 @@ package com.dsl.anime.tracker.validators;
 import com.dsl.anime.tracker.constraints.Anime;
 import com.dsl.anime.tracker.entity.AnimeEntity;
 import com.dsl.anime.tracker.repository.AnimeRepository;
+import com.dsl.anime.tracker.rest.dto.AnimeDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.Objects;
+import java.util.Optional;
 
-public class AnimeValidator implements ConstraintValidator<Anime, AnimeEntity>
+public class AnimeValidator implements ConstraintValidator<Anime, AnimeDetails>
 {
     @Autowired
     private AnimeRepository animeRepository;
 
     @Override
-    public boolean isValid(AnimeEntity animeEntity, ConstraintValidatorContext constraintValidatorContext)
+    public boolean isValid(AnimeDetails animeDetails, ConstraintValidatorContext constraintValidatorContext)
     {
-        return !animeRepository.findByName(animeEntity.getName()).isPresent();
+        if(Objects.nonNull(animeDetails.getId()))
+        {
+            Optional<AnimeEntity> entity = animeRepository.findByName(animeDetails.getName());
+            return !entity.isPresent() || Objects.equals(entity.get().getId(), animeDetails.getId());
+        }
+
+        return !animeRepository.findByName(animeDetails.getName()).isPresent();
     }
 }
