2cc3a860ec54ecaa5d09480effaa3b1d9330a2f0
dslsyeoh <syeoh91x@gmail.com>
2019-09-04 02:03:47 +0800

    add SecurityConfig

---
+++ src/main/java/com/dsl/anime/tracker/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2019. All rights reserved
+ */
+
+package com.dsl.anime.tracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableGlobalAuthentication
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Bean("passwordEncoder")
+    public BCryptPasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.csrf().disable().authorizeRequests().antMatchers("*").permitAll();
+    }
+}
